40ccfc47a200f17ccc6a5fa29a0e28d3f0d00445
ganjarwira87 <92734617+ganjarwira87@users.noreply.github.com>
2023-01-17 14:39:15 +0700

    ganjar v

--- 3tab/index.php
+++ 3tab/index.php
@@ -1,19 +1,15 @@
 <?php
-
     include "koneksi.php";
-
 ?>
 <h2>Latihan</h2>
 <table border="1">
-
-   
     <thead>
     <tr>
         <td>No</td>
         <td>Nama Pegawai</td>
         <td>Nama Bidang</td>
+        <td>Keterangan</td>
         <td>Jumlah Pendapatan</td>
-        <td>keterangan</td>
 
     </tr>
     </thead>
@@ -23,20 +19,32 @@
         INNER JOIN master_bidang ON master_pegawai.id_bidang = master_bidang.id_bidang 
         INNER JOIN master_pendapatan ON master_pegawai.id_pegawai = master_pendapatan.id_pegawai 
         GROUP BY master_pegawai.id_pegawai;";
-       $no=1;
+        $no=1;
         $call = mysqli_query($mysqli, $sql);
-        while($dan=mysqli_fetch_array($call)) {
+        while($dan=mysqli_fetch_array($call)){
             $nama_bidang=$dan['nama_bidang'];
             $nama_pegawai=$dan['nama_pegawai'];
             $jumlah_pendapatan=$dan['jumlah_pendapatan'];
+            $keterangan=$dan['jumlah_pendapatan'];
+            if ($jumlah_pendapatan < "50000"){
+                $keterangan = "kurang";
+                $color = "style = 'background-color:red;'";
+            }elseif ($jumlah_pendapatan < "100000"){
+                $keterangan ="standar lah";
+                $color = "style = 'background-color:blue'";
+            }else{
+                $keterangan = "melampaui target";
+                $color = "style = 'background-color:green'";
+            }
     ?>
-    <tbody>
+    <tbody <?=$color ?> >
         <tr>
             <td><?php echo $no ?></td>
             <td><?php echo $nama_bidang ?> </td>
             <td><?php echo $nama_pegawai ?></td>
+            <td><?php echo $keterangan ?></td>
             <td><?php echo $jumlah_pendapatan ?></td>
-            <td></td>
+            
         </tr>
     </tbody>
     <?php
